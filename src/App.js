7a565a862311e6438be7278abe45bb6f23d86bd2
Nagpal45 <nagpal4503@gmail.com>
2023-07-29 10:27:59 +0530

    completed user pages

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import Sidebar from "./components/sidebar/sidebar";
 import Topbar from "./components/topbar/topbar";
 import "./app.css";
 import Home from "./pages/home/home";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import UserList from "./pages/userList/userList";
+import User from "./pages/user/user";
+import NewUser from "./pages/newUser/newUser";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Topbar />
       <div className="container">
         <Sidebar/>
-        <Home/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/user/:userId" element={<User/>} />
+          <Route path="/newUser" element={<NewUser/>} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/dummyData.js
+++ src/dummyData.js
@@ -53,3 +53,78 @@ export const userData = [
     },
 ];
 
+export const rows = [
+    { 
+        id: 1,
+        userName: 'Jon Snow',
+        avatar: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        email:'jon@gmail.com',
+        status: 'active',
+        transaction: '$120'
+    },
+    {
+        id: 2,
+        userName: 'Jon Snow',
+        avatar: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        email:'jon@gmail.com',
+        status: 'active',
+        transaction: '$120'
+    },
+    {
+        id: 3,
+        userName: 'Jon Snow',
+        avatar: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        email:'jon@gmail.com',
+        status: 'active',
+        transaction: '$120'
+    },
+    {
+        id: 4,
+        userName: 'Jon Snow',
+        avatar: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        email:'jon@gmail.com',
+        status: 'active',
+        transaction: '$120'
+    },
+    {
+        id: 5,
+        userName: 'Jon Snow',
+        avatar: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        email:'jon@gmail.com',
+        status: 'active',
+        transaction: '$120'
+    },
+    {
+        id: 6,
+        userName: 'Jon Snow',
+        avatar: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        email:'jon@gmail.com',
+        status: 'active',
+        transaction: '$120'
+    },
+    {
+        id: 7,
+        userName: 'Jon Snow',
+        avatar: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        email:'jon@gmail.com',
+        status: 'active',
+        transaction: '$120'
+    },
+    {
+        id: 8,
+        userName: 'Jon Snow',
+        avatar: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        email:'jon@gmail.com',
+        status: 'active',
+        transaction: '$120'
+    },
+    {
+        id: 9,
+        userName: 'Jon Snow',
+        avatar: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        email:'jon@gmail.com',
+        status: 'active',
+        transaction: '$120'
+    },
+    
+];
